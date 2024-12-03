a85c4d2baa5273d7273d54d73a261c6fc819cef5
typecaster <nawazasif2886@gmail.com>
2024-12-03 23:06:27 +0530

    Till API creation

---
+++ src/main/java/com/social/media/models/SocialProfile.java
@@ -0,0 +1,24 @@
+package com.social.media.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SocialProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "social_user")
+    @JsonIgnore
+    private SocialUser user;
+
+    private String description;
+}
